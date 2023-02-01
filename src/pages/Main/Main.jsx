265b20e601a2a6a5b3a9d00eb9662294004ffaf7
Isai Martinez <isai.martinez@yeico.com>
2023-02-01 16:05:10 -0600

    Reset and "Autorizó" Field

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,19 +2,30 @@ import React from 'react'
 import {QRCode, CustomInput} from 'components'
 import Button from '@mui/material/Button';
 import { useMain } from './useMain';
+import { FaTrash } from 'react-icons/fa';
 
 const Main = () => {
-  const {qr, visitante, setVisitante, noCasa, setNoCasa, vehiculo, setVehiculo, vehiculoColor, setVehiculoColor, handleGenerateQr, isBtnEnabled} = useMain()
+  const {qr, visitante, setVisitante, noCasa, setNoCasa, vehiculo, setVehiculo, vehiculoColor, setVehiculoColor, handleGenerateQr, 
+    isBtnEnabled, autorizo, setAutorizo, handleReset
+  } = useMain()
 
   return (
-    <div className='flex flex-col w-full h-screen m-auto justify-center items-center gap-2'> 
+    <div className='flex flex-col w-full h-screen m-auto justify-center items-center gap-3'> 
       <CustomInput label={"Visitante"} onChange={setVisitante} value={visitante}/>
       <CustomInput label={"No. Casa"} onChange={setNoCasa} value={noCasa}/>
       <CustomInput label={"Tipo Vehiculo"} onChange={setVehiculo} value={vehiculo}/>
       <CustomInput label={"Color"} onChange={setVehiculoColor} value={vehiculoColor}/>
-      <div className='flex flex-col mt-5  gap-5'>
+      <CustomInput label={"Autorizó"} onChange={setAutorizo} value={autorizo}/>
+      <div className='flex flex-col mt-3  gap-5'>
         <div className='flex items-center justify-center'>
+          {
+            qr ? (
+              <Button variant="outlined" color='error' onClick={handleReset} startIcon={<FaTrash />} >Reset</Button>
+            ) : (
           <Button variant="outlined" onClick={handleGenerateQr} disabled={!isBtnEnabled}>Generate Qr</Button>
+
+            )
+          }
         </div>
         {qr && <QRCode value={qr} />}
       </div>
--- src/pages/Main/useMain.js
+++ src/pages/Main/useMain.js
@@ -2,29 +2,41 @@ import React,{useState, useEffect} from 'react'
 import { v4 as uuidv4 } from 'uuid';
 
 export const useMain = () => {
-    const [qr, setQr] = useState("")
+    const [qr, setQr] = useState(null)
     const [visitante, setVisitante] = useState("")
     const [noCasa, setNoCasa] = useState("")
     const [vehiculo, setVehiculo] = useState("")
     const [vehiculoColor, setVehiculoColor] = useState("")
+    const [autorizo, setAutorizo] = useState("")
     const [isBtnEnabled, setIsBtnEnabled] = useState(false);
     
     useEffect(() => {
-        if(visitante.length == 0 || noCasa.length == 0 || vehiculo.length == 0 || vehiculoColor.length == 0){
+        if(visitante.length == 0 || noCasa.length == 0 || vehiculo.length == 0 || vehiculoColor.length == 0 || autorizo.length == 0){
             setIsBtnEnabled(false)
         } else {
             setIsBtnEnabled(true)
         }
-    }, [visitante, noCasa, vehiculo, vehiculoColor]);
+    }, [visitante, noCasa, vehiculo, vehiculoColor, autorizo]);
 
 
     const handleGenerateQr = () => {
         const id = uuidv4().substring(0,7)
-        let str = id+","+visitante+","+noCasa+","+vehiculo+","+vehiculoColor
+        let str = id+","+visitante+","+noCasa+","+vehiculo+","+vehiculoColor+","+autorizo
         console.log("str", str)
         setQr(btoa(str))
     }
+
+    const handleReset = () => {
+      setVisitante("")
+      setNoCasa("")
+      setVehiculo("")
+      setVehiculoColor("")
+      setAutorizo("")
+      setQr(null)
+    }
     
 
-    return {qr, setQr, visitante, setVisitante, noCasa, setNoCasa, vehiculo, setVehiculo, vehiculoColor, setVehiculoColor, handleGenerateQr, isBtnEnabled}
+    return {qr, setQr, visitante, setVisitante, noCasa, setNoCasa, vehiculo, setVehiculo, vehiculoColor, setVehiculoColor, handleGenerateQr, 
+      isBtnEnabled, autorizo, setAutorizo, handleReset
+    }
 }

6a6b3a51dbea44bfe6c1198cc36af93781be0a4b
Isai Martinez <isai.martinez@yeico.com>
2023-01-31 19:19:06 -0600

    test

--- src/components/QRCode/QRCode.jsx
+++ src/components/QRCode/QRCode.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import QRCode from "react-qr-code";
-
+import {WhatsappShareButton, WhatsappIcon} from 'react-share'
 const QRCodeComponent = ({value}) => {
+
+  const clipboard = () => {
+    navigator.clipboard.writeText(value);
+  }
+
   return (
-    <div className='w-52 h-52'>
-      <QRCode
-        size={100}
-        style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-        value={value}
-        viewBox={`0 0 256 256`}
-      />
+    <div className='flex flex-row gap-5'>
+      <div className='flex w-52 h-52'>
+        <QRCode
+          size={100}
+          style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+          value={value}
+          viewBox={`0 0 256 256`}
+        />
+      </div>
+      <div className='flex justify-center items-center cursor-pointer'
+        onClick={clipboard}
+      >
+        Copiar
+      </div>
+
     </div>
   )
 }
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,8 +1,7 @@
-import React,{useState} from 'react'
+import React from 'react'
 import {QRCode, CustomInput} from 'components'
 import Button from '@mui/material/Button';
 import { useMain } from './useMain';
-
 
 const Main = () => {
   const {qr, visitante, setVisitante, noCasa, setNoCasa, vehiculo, setVehiculo, vehiculoColor, setVehiculoColor, handleGenerateQr, isBtnEnabled} = useMain()
